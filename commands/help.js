2bbf32c55e7b43642f990ef3328cac63eabf9240
MatthewSK90 <matusko.ceresnik@gmail.com>
2020-08-25 13:19:37 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,9 +8,11 @@ module.exports = {
     let commands = message.client.commands.array();
 
     let helpEmbed = new MessageEmbed()
-      .setTitle("Bluetooth Speaker Help")
+      .setTitle("Invite Me")
       .setDescription("List of all commands")
-      .setColor("#F8AA2A");
+      .setColor("#F8AA2A")
+      .setURL('https://discord.com/oauth2/authorize?client_id=741597659969617991&scope=bot&permissions=36908488')
+      .setAuthor('Bluetooth Speaker', 'https://images.discordapp.net/avatars/741597659969617991/0130072737b99aa90203ea5b48b398f4.png?size=512', 'https://discord.com/oauth2/authorize?client_id=741597659969617991&scope=bot&permissions=36908488');
 
     commands.forEach((cmd) => {
       helpEmbed.addField(
