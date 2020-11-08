fcc3a16421c8ec4b996a6468b6db5f9ca9cdff2b
MatthewSK90 <matusko.ceresnik@gmail.com>
2020-11-08 17:04:03 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,11 +8,11 @@ module.exports = {
     let commands = message.client.commands.array();
 
     let helpEmbed = new MessageEmbed()
-      .setTitle("Invite Me")
-      .setDescription("List of all commands")
-      .setColor("#00b0f4")
-      .setURL('https://discord.com/oauth2/authorize?client_id=741597659969617991&scope=bot&permissions=36908488')
-      .setAuthor('Bluetooth Speaker', 'https://images.discordapp.net/avatars/741597659969617991/0130072737b99aa90203ea5b48b398f4.png?size=512', 'https://discord.com/oauth2/authorize?client_id=741597659969617991&scope=bot&permissions=36908488');
+    .setTitle("Invite Me")
+    .setDescription("List of all commands")
+    .setColor("#00b0f4")
+    .setURL('https://discord.com/oauth2/authorize?client_id=741597659969617991&scope=bot&permissions=36908488')
+    .setAuthor('Bluetooth Speaker', 'https://images.discordapp.net/avatars/741597659969617991/0130072737b99aa90203ea5b48b398f4.png?size=512', 'https://discord.com/oauth2/authorize?client_id=741597659969617991&scope=bot&permissions=36908488');
 
     commands.forEach((cmd) => {
       helpEmbed.addField(
