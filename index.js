2b1cf6ecc234e1f339a43ad3056401c9b49b1798
MatthewSK90 <matusko.ceresnik@gmail.com>
2020-11-08 17:04:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
+/**
+ * Module Imports
+ */
 const { Client, Collection } = require("discord.js");
 const { readdirSync } = require("fs");
 const { join } = require("path");
-const { TOKEN, PREFIX } = require("./config.json");
+
+let TOKEN, PREFIX;
+try {
+  const config = require("./config.json");
+  TOKEN = config.TOKEN;
+  PREFIX = config.PREFIX;
+} catch (error) {
+  TOKEN = process.env.TOKEN;
+  PREFIX = process.env.PREFIX;
+}
 
 const client = new Client({ disableMentions: "everyone" });
 
-client.login(process.env.TOKEN);
+client.login(TOKEN);
 client.commands = new Collection();
 client.prefix = PREFIX;
 client.queue = new Map();
 const cooldowns = new Collection();
 const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 
-
+/**
+ * Client Events
+ */
 client.on("ready", () => {
   console.log(`${client.user.username} ready!`);
-  client.user.setActivity(`${PREFIX}help`);
+  client.user.setActivity(`${PREFIX}help and ${PREFIX}play`, { type : "LISTENING" });
 });
 client.on("warn", (info) => console.log(info));
 client.on("error", console.error);
 
+/**
+ * Import all commands
+ */
 const commandFiles = readdirSync(join(__dirname, "commands")).filter((file) => file.endsWith(".js"));
 for (const file of commandFiles) {
   const command = require(join(__dirname, "commands", `${file}`));
